8f94bc36c84aef3239e76c7ee1c1acc76d09d4f8
Zhupanov Dmitry <zhupanov.dmitry@gmail.com>
2021-08-17 23:44:03 +0300

    Date literal grammar test

--- bsl-core/src/test/java/org/zhupanovdm/sonar/bsl/grammar/literals/DateTest.java
+++ bsl-core/src/test/java/org/zhupanovdm/sonar/bsl/grammar/literals/DateTest.java
@@ -13,9 +13,13 @@ public class DateTest {
     public void test() {
         Assertions.assertThat(g.rule(BslGrammar.DATE))
                 .matches("'20210807224656'")
+                .matches("'00010101000000'")
                 .matches("'20210807'")
+                .matches("'00010101'")
                 .matches("'2021.08.07 22:46:56'")
+                .matches("'0001.01.01 00:00:00'")
                 .matches("'2021.08.07'")
+                .matches("'0001.01.01'")
                 .notMatches("'202108072246'")
                 .notMatches("'202108'")
                 .notMatches("'2021.08.07 22:46'")
