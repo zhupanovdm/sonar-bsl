64b76c2c5b14ffc2443f9e0cc83aa8298e8d0ce5
Zhupanov Dmitry <zhupanov.dmitry@gmail.com>
2021-08-18 01:46:58 +0300

    Raise statement grammar test

---
+++ bsl-core/src/test/java/org/zhupanovdm/sonar/bsl/grammar/statements/RaiseStatementTest.java
@@ -0,0 +1,19 @@
+package org.zhupanovdm.sonar.bsl.grammar.statements;
+
+import org.junit.Test;
+import org.sonar.sslr.parser.LexerlessGrammar;
+import org.sonar.sslr.tests.Assertions;
+import org.zhupanovdm.sonar.bsl.grammar.BslGrammar;
+
+public class RaiseStatementTest {
+
+    private final LexerlessGrammar g = BslGrammar.createGrammar();
+
+    @Test
+    public void test() {
+        Assertions.assertThat(g.rule(BslGrammar.RAISE_STATEMENT))
+                .matches("Raise \"Text\"")
+                .matches("ВызватьИсключение \"Текст\"");
+    }
+
+}
