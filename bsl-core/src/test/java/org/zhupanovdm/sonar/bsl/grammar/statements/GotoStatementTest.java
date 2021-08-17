c5c3f29421b39ea29737de7ba573ad8009900572
Zhupanov Dmitry <zhupanov.dmitry@gmail.com>
2021-08-18 02:15:43 +0300

    Goto statement grammar test

---
+++ bsl-core/src/test/java/org/zhupanovdm/sonar/bsl/grammar/statements/GotoStatementTest.java
@@ -0,0 +1,19 @@
+package org.zhupanovdm.sonar.bsl.grammar.statements;
+
+import org.junit.Test;
+import org.sonar.sslr.parser.LexerlessGrammar;
+import org.sonar.sslr.tests.Assertions;
+import org.zhupanovdm.sonar.bsl.grammar.BslGrammar;
+
+public class GotoStatementTest {
+
+    private final LexerlessGrammar g = BslGrammar.createGrammar();
+
+    @Test
+    public void test() {
+        Assertions.assertThat(g.rule(BslGrammar.GOTO_STATEMENT))
+                .matches("Goto ~label")
+                .matches("Перейти ~метка");
+    }
+
+}
