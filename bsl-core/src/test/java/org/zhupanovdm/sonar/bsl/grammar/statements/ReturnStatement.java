89eda5606f26f19b60a0cca67b9617d20adae0c1
Zhupanov Dmitry <zhupanov.dmitry@gmail.com>
2021-08-18 00:56:35 +0300

    Return statement grammar test

---
+++ bsl-core/src/test/java/org/zhupanovdm/sonar/bsl/grammar/statements/ReturnStatement.java
@@ -0,0 +1,20 @@
+package org.zhupanovdm.sonar.bsl.grammar.statements;
+
+import org.junit.Test;
+import org.sonar.sslr.parser.LexerlessGrammar;
+import org.sonar.sslr.tests.Assertions;
+import org.zhupanovdm.sonar.bsl.grammar.BslGrammar;
+
+public class ReturnStatement {
+
+    private final LexerlessGrammar g = BslGrammar.createGrammar();
+
+    @Test
+    public void test() {
+        Assertions.assertThat(g.rule(BslGrammar.RETURN_STATEMENT))
+                .matches("return")
+                .matches("возврат")
+                .matches("return foo");
+    }
+
+}
