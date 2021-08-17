1e0d0ca8a3ace3e82d62fc3d8ee49ef1b24424c7
Zhupanov Dmitry <zhupanov.dmitry@gmail.com>
2021-08-18 02:10:55 +0300

    Add handler statement grammar test class renamed

--- bsl-core/src/test/java/org/zhupanovdm/sonar/bsl/grammar/statements/AddHandlerStatementTest.java
+++ bsl-core/src/test/java/org/zhupanovdm/sonar/bsl/grammar/statements/AddHandlerStatementTest.java
@@ -5,7 +5,7 @@ import org.sonar.sslr.parser.LexerlessGrammar;
 import org.sonar.sslr.tests.Assertions;
 import org.zhupanovdm.sonar.bsl.grammar.BslGrammar;
 
-public class AddHandlerStatement {
+public class AddHandlerStatementTest {
 
     private final LexerlessGrammar g = BslGrammar.createGrammar();
 
