f908a4f8bb7d175d6a54c45fe5d19acc728269a5
Zhupanov Dmitry <zhupanov.dmitry@gmail.com>
2021-08-17 23:32:55 +0300

    New expression grammar test

---
+++ bsl-core/src/test/java/org/zhupanovdm/sonar/bsl/grammar/expressions/NewExpressionTest.java
@@ -0,0 +1,22 @@
+package org.zhupanovdm.sonar.bsl.grammar.expressions;
+
+import org.junit.Test;
+import org.sonar.sslr.parser.LexerlessGrammar;
+import org.sonar.sslr.tests.Assertions;
+import org.zhupanovdm.sonar.bsl.grammar.BslGrammar;
+
+public class NewExpressionTest {
+
+    private final LexerlessGrammar g = BslGrammar.createGrammar();
+
+    @Test
+    public void test() {
+        Assertions.assertThat(g.rule(BslGrammar.NEW_EXPRESSION))
+                .matches("new Structure")
+                .matches("new Structure()")
+                .matches("new Structure(\"field\",, 10 + 1,)")
+                .matches("new Array(10)")
+                .matches("новый Структура");
+    }
+
+}
