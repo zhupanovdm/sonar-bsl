41a3678a4cd13c0cebea212a88465176fe803e60
Zhupanov Dmitry <zhupanov.dmitry@gmail.com>
2021-08-17 22:17:48 +0300

    Identifier test

--- bsl-core/src/test/java/org/zhupanovdm/sonar/bsl/grammar/expressions/IdentifierTest.java
+++ bsl-core/src/test/java/org/zhupanovdm/sonar/bsl/grammar/expressions/IdentifierTest.java
@@ -1,8 +1,9 @@
-package org.zhupanovdm.sonar.bsl.grammar;
+package org.zhupanovdm.sonar.bsl.grammar.expressions;
 
 import org.junit.Test;
 import org.sonar.sslr.parser.LexerlessGrammar;
 import org.sonar.sslr.tests.Assertions;
+import org.zhupanovdm.sonar.bsl.grammar.BslGrammar;
 
 public class IdentifierTest {
 
@@ -12,13 +13,19 @@ public class IdentifierTest {
     public void test() {
         Assertions.assertThat(g.rule(BslGrammar.IDENTIFIER))
                 .matches("_")
+                .matches("_1")
+                .matches("_яz")
                 .matches("identifier")
                 .matches("идентификатор")
+                .matches("пока123")
+                .matches("while123")
                 .notMatches("for")
+                .notMatches("For")
                 .notMatches("для")
                 .notMatches("Для")
-                .matches("пока123")
-                .matches("while123");
+                .notMatches("123")
+                .notMatches("123qwer")
+                .notMatches("123йцук");
     }
 
 }
