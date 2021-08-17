b45149e3953ce270fbc994b09944242801fe5733
Zhupanov Dmitry <zhupanov.dmitry@gmail.com>
2021-08-17 23:46:19 +0300

    Primary expression grammar test

---
+++ bsl-core/src/test/java/org/zhupanovdm/sonar/bsl/grammar/expressions/PrimaryExpressionTest.java
@@ -0,0 +1,22 @@
+package org.zhupanovdm.sonar.bsl.grammar.expressions;
+
+import org.junit.Test;
+import org.sonar.sslr.parser.LexerlessGrammar;
+import org.sonar.sslr.tests.Assertions;
+import org.zhupanovdm.sonar.bsl.grammar.BslGrammar;
+
+public class PrimaryExpressionTest {
+
+    private final LexerlessGrammar g = BslGrammar.createGrammar();
+
+    @Test
+    public void primaryExpression() {
+        Assertions.assertThat(g.rule(BslGrammar.PRIMARY_EXPRESSION))
+                .matches("1")
+                .matches("foo")
+                .matches("null")
+                .matches("(1 + 1)")
+                .notMatches("1 + 1");
+    }
+
+}
