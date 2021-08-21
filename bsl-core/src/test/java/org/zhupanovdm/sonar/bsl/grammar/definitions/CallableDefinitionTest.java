ae1c954d1cfe692b9102963cd2915a6dc8cebd68
Zhupanov Dmitry <zhupanov.dmitry@gmail.com>
2021-08-21 16:51:58 +0300

    Callable definition grammar rules test

---
+++ bsl-core/src/test/java/org/zhupanovdm/sonar/bsl/grammar/definitions/CallableDefinitionTest.java
@@ -0,0 +1,72 @@
+package org.zhupanovdm.sonar.bsl.grammar.definitions;
+
+import org.junit.Test;
+import org.sonar.sslr.parser.LexerlessGrammar;
+import org.zhupanovdm.sonar.bsl.grammar.BslGrammar;
+
+import static org.sonar.sslr.tests.Assertions.assertThat;
+import static org.zhupanovdm.sonar.bsl.grammar.BslGrammar.*;
+
+public class CallableDefinitionTest {
+
+    private final LexerlessGrammar g = BslGrammar.createGrammar();
+
+    @Test
+    public void definition() {
+        assertThat(g.rule(CALLABLE_DEFINITION))
+                .matches("function a() export;")
+                .matches("procedure a() export;")
+                .notMatches("function a() export");
+    }
+
+    @Test
+    public void function() {
+        assertThat(g.rule(FUNC_DEFINITION))
+                .matches("function a() export endfunction")
+                .matches("&AtServer function a() endfunction")
+                .matches("&AtServer\nfunction\na()\nfoo()\nendfunction")
+                .notMatches("function a() export endfunction;");
+    }
+
+    @Test
+    public void procedure() {
+        assertThat(g.rule(PROC_DEFINITION))
+                .matches("procedure a() export endprocedure")
+                .matches("&AtServer procedure a() endprocedure")
+                .matches("&AtServer\nprocedure\na()\nfoo()\nendprocedure")
+                .notMatches("procedure a() export endprocedure;");
+    }
+
+    @Test
+    public void signature() {
+        assertThat(g.rule(CALLABLE_SIGNATURE))
+                .matches("a() export")
+                .matches("a\n()\nexport")
+                .matches("a()");
+    }
+
+    @Test
+    public void parameters() {
+        assertThat(g.rule(CALLABLE_PARAMETERS))
+                .matches("()")
+                .matches("(a)")
+                .matches("(a, b, c)")
+                .matches("(\na\n,\nb,\nn)")
+                .notMatches("(a + b)")
+                .notMatches("(,)")
+                .notMatches("(a b)");
+    }
+
+    @Test
+    public void parameter() {
+        assertThat(g.rule(PARAMETER))
+                .matches("a")
+                .matches("a = null")
+                .matches("val a")
+                .matches("val a = null")
+                .matches("val\na\n=\nnull")
+                .notMatches("")
+                .notMatches("a = foo()");
+    }
+
+}
