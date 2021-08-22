b4a56f45181edf8fe7b6d754017c3bb9eeea06d0
Zhupanov Dmitry <zhupanov.dmitry@gmail.com>
2021-08-22 16:54:36 +0300

    Var block grammar test

--- bsl-core/src/test/java/org/zhupanovdm/sonar/bsl/grammar/BslVarBlockTest.java
+++ bsl-core/src/test/java/org/zhupanovdm/sonar/bsl/grammar/BslVarBlockTest.java
@@ -24,23 +24,26 @@ public class BslVarBlockTest {
                 .matches("#if server then #endif")
                 .matches("#if server then var a; var b; #endif")
                 .matches(
-                        "var a;\n" +
-                        "var b;\n" +
                         "#if server then\n" +
-                        "\tvar c;\n" +
+                        "#region a #endregion\n" +
+                        "#elsif client then\n" +
+                        "#region b #endregion\n" +
+                        "#endif")
+                .matches(
+                        "var a;\n" +
+                        "#if server then\n" +
+                        "\tvar b;\n" +
                         "\t#if server then\n" +
-                        "\t\tvar d;\n" +
+                        "\t\tvar c;\n" +
                         "\t#elsif client then\n" +
-                        "\t\tvar e;\n" +
-                        "\t\tvar f;\n" +
+                        "\t\tvar d;\n" +
                         "\t#elsif client then\n" +
                         "\t#endif\n" +
                         "#elsif client then\n" +
                         "\tvar j;\n" +
                         "#elsif client then\n" +
                         "#endif\n" +
-                        "var k;"
-                );
+                        "var k;");
     }
 
     @Test
@@ -49,8 +52,22 @@ public class BslVarBlockTest {
                 .matches("#region a #endregion")
                 .matches("#region a var a; var b; #endregion")
                 .matches("#region a #region b #endregion #endregion")
-
-        ;
+                .matches(
+                        "#region a\n" +
+                        "#if server then\n" +
+                        "#elsif client then\n" +
+                        "#endif\n" +
+                        "#endregion")
+                .matches(
+                        "var a;\n" +
+                        "#region b\n" +
+                        "var b;\n" +
+                        "#region c\n" +
+                        "var d;\n" +
+                        "#endregion\n" +
+                        "var e;\n" +
+                        "#endregion\n" +
+                        "var f;");
     }
 
 }
